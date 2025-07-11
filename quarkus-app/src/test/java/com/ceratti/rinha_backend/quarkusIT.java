9461764ab5f84bcfb07e20c56586fe88d264a334
gabriel ceratti <gabriel@zeta.com.br>
2025-07-11 17:00:43 -0300

    feat: Initialize Quarkus application with Docker support and REST endpoint
    - Added Maven wrapper script for project build management.
    - Created POM file for Quarkus application with dependencies and build configuration.
    - Implemented Dockerfiles for JVM, legacy JAR, native, and micro-native builds.
    - Developed a simple REST endpoint returning "Hello RESTEasy".
    - Configured application properties for Redis connection and HTTP port.
    - Added integration and unit tests for the REST endpoint.

---
+++ quarkus-app/src/test/java/com/ceratti/rinha_backend/quarkusIT.java
@@ -0,0 +1,8 @@
+package com.ceratti.rinha_backend;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class quarkusIT extends quarkusTest {
+    // Execute the same tests but in packaged mode.
+}
